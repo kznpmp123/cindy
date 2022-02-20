ebc3982ecab85ec84c682ad43ee4f193fe4e43d8
zarnibwar <kznpmp@gmail.com>
2022-02-20 23:32:37 +0630

    Add all my files

---
+++ lib/screen/item_upload_screen.dart
@@ -0,0 +1,332 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:kozarni_ecome/controller/home_controller.dart';
+import 'package:kozarni_ecome/controller/upload_controller.dart';
+import 'package:kozarni_ecome/data/constant.dart';
+import 'package:kozarni_ecome/model/item.dart';
+
+class UploadItem extends StatefulWidget {
+  const UploadItem({Key? key}) : super(key: key);
+
+  @override
+  State<UploadItem> createState() => _UploadItemState();
+}
+
+class _UploadItemState extends State<UploadItem> {
+  final UploadController controller = Get.find();
+  final HomeController homecontroller = Get.find();
+
+  @override
+  void dispose() {
+    homecontroller.setEditItem(
+      ItemModel(
+        photo: '',
+        photo2: '',
+        photo3: '',
+        brand: '',
+        deliverytime: '',
+        discountprice: 0,
+        name: '',
+        price: 0,
+        desc: '',
+        color: '',
+        size: '',
+        star: 0,
+        category: '',
+      ),
+    );
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: scaffoldBackground,
+      appBar: AppBar(
+        title: Text("𝐂𝐢𝐧𝐝𝐲 Branded Export Fashion", style: TextStyle(color: Colors.black, fontSize: 14),),
+        elevation: 5,
+        backgroundColor: detailBackgroundColor,
+        leading: IconButton(
+          onPressed: Get.back,
+          icon: Icon(
+            Icons.arrow_back,
+            color: appBarTitleColor,
+          ),
+        ),
+      ),
+      body: Form(
+        key: controller.form,
+        child: ListView(
+          children: [
+            // GestureDetector(
+            //   onTap: controller.pickImage,
+            //   child: Container(
+            //     width: double.infinity,
+            //     height: 200,
+            //     margin: EdgeInsets.only(
+            //       left: 20,
+            //       right: 20,
+            //       top: 20,
+            //     ),
+            //     child: Card(
+            //       child: Obx(
+            //         () => controller.filePath.isEmpty
+            //             ? Center(
+            //                 child: Icon(
+            //                   Icons.image,
+            //                 ),
+            //               )
+            //             : ClipRRect(
+            //                 borderRadius: BorderRadius.all(
+            //                   Radius.circular(7),
+            //                 ),
+            //                 child: Image.file(
+            //                   File(
+            //                     controller.filePath.value,
+            //                   ),
+            //                   fit: BoxFit.cover,
+            //                 ),
+            //               ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.photoController,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'Photo Link',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.photo2Controller,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'Photo Link 2',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.photo3Controller,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'Photo Link 3',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.nameController,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'Product အမည်',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.brandController,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'Brand အမည်',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                // textInputAction: TextInputAction.done,
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.newline,
+                minLines: 1,
+                maxLines: null,
+
+                controller: controller.desc,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'အသေးစိတ်ဖော်ပြချက်',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.discountpriceController,
+                validator: controller.validator,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'ပုံမှန် စျေးနှုန်း',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.priceController,
+                validator: controller.validator,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'ရောင်းချမည့် စျေးနှုန်း',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.deliverytimeController,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'Delivery Time',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.colorController,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'အရောင်',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.sizeController,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'အရွယ်အစား',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.starController,
+                validator: controller.validator,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'Star',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                controller: controller.categoryController,
+                validator: controller.validator,
+                decoration: InputDecoration(
+                  hintText: 'အမျိုးအစား',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              margin: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+              child: ElevatedButton(
+                style: buttonStyle,
+                onPressed: controller.upload,
+                child: Obx(
+                  () => controller.isUploading.value
+                      ? CircularProgressIndicator(
+                          color: scaffoldBackground,
+                        )
+                      : Text(homecontroller.editItem.value.id != null
+                          ? "Edit"
+                          : "upload"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/view/home.dart
@@ -0,0 +1,107 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:get/get.dart';
+import 'package:kozarni_ecome/controller/home_controller.dart';
+import 'package:kozarni_ecome/data/constant.dart';
+import 'package:kozarni_ecome/routes/routes.dart';
+import 'package:kozarni_ecome/widgets/home_category.dart';
+import 'package:kozarni_ecome/widgets/home_items.dart';
+import 'package:kozarni_ecome/widgets/home_pickup.dart';
+
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeController controller = Get.find();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Obx(
+        () => controller.isSearch.value
+            ? StaggeredGridView.countBuilder(
+          staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
+          shrinkWrap: true,
+          physics: ScrollPhysics(),
+
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+                itemCount: controller.searchitems.length,
+                itemBuilder: (_, i) => GestureDetector(
+                  onTap: () {
+                    controller.setSelectedItem(controller.searchitems[i]);
+                    Get.toNamed(detailScreen);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(),
+                      child: Container(
+                        color: Colors.white,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Hero(
+                              tag: controller.getItems()[i].photo,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: CachedNetworkImage(
+                                  imageUrl: controller.getItems()[i].photo,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+
+                            SizedBox(
+                              height: 5,
+                            ),
+
+                            Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 5, left: 24, right: 20),
+                                  child: Text(
+                                    controller.getItems()[i].name,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 5, bottom: 5, left: 30, right: 20),
+                                  child: Text(
+                                    "${controller.getItems()[i].price} Kyats",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            : ListView(
+                children: [
+                  // HomePickUp(),
+                  HomeCategory(),
+                  HomeItems(),
+                ],
+              ),
+      ),
+    );
+  }
+}
